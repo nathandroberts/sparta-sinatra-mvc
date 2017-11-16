cb66d407381983288228db6996b6af172d68cd1e
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-16 16:15:13 +0000

    simple template created

--- controllers/photos_controller.rb
+++ controllers/photos_controller.rb
@@ -2,13 +2,37 @@ class PhotosController < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+  #sets root as the parent directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+  set :views, Proc.new { File.join(root, 'views')}
+  $photos = [{
+	 id: 0,
+	 title: "Photo 1",
+	 body: "This is the first post",
+   source: "https://78.media.tumblr.com/avatar_7340c7bed74b_128.png"
+},
+{
+    id: 1,
+    title: "Photo 2",
+    body: "This is the second post",
+    source: "https://is2-ssl.mzstatic.com/image/thumb/Purple3/v4/99/3d/7d/993d7d30-a2f9-085f-8b70-ff71f1eee2bf/source/256x256bb.jpg"
+},
+{
+    id: 2,
+    title: "Photo 3",
+    body: "This is the third post",
+    source: "https://static-s.aa-cdn.net/img/ios/905870222/30e83b11b5a9a086abd5f8a4acab174d?v=1"
+}];
 
   get '/' do
-    'Hello world - abcdefghijklmnopqrstuvwxyz'
+    @title = 'Photo Homepage'
+    erb :'photos/home'
   end
 
   get '/photos' do
-    'this is the photos page'
+    @title = 'Photos'
+    @photos = $photos
+    erb :'photos/index'
   end
 
   get '/photos/new' do
@@ -20,11 +44,15 @@ class PhotosController < Sinatra::Base
   end
 
   get '/photos/:id' do
-    id = params[:id]
-    "SHOW #{id}"
+    id = params[:id].to_i
+    @photo = $photos[id]
+    erb :'photos/show'
   end
   get '/photos/:id/edit' do
-    "edit photo #{params[:id]}"
+    @paragraph = 'edit this photo'
+    id = params[:id].to_i
+    @photo = $photos[id]
+    erb :'photos/edit'
   end
   put '/photos/:id' do
     "update photo#{params[:id]}"
